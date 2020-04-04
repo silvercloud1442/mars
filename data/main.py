de8e4b16eb109f52dc7b9ef2fff78ad90a268c3e
silvercloud1442 <47461834+silvercloud1442@users.noreply.github.com>
2020-04-04 10:18:52 +0300

    Add files via upload

--- data/main.py
+++ data/main.py
@@ -1,14 +1,25 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.utils import redirect
+import os
 from data.db_session import global_init, create_session
 from data.jobs import Jobs
+from data.users import User
+from data.RegisterForm import RegisterForm
+from data.LoginForm import LoginForm
+from data.JobsForm import JobsForm
+
 
 app = Flask(__name__)
-
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+login_manager = LoginManager()
+login_manager.init_app(app)
+global_init('db/dbase.sqlite')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    global_init('db/dbase.sqlite')
     session = create_session()
     jobs_list = []
     for jobs in session.query(Jobs).all():
@@ -81,6 +92,82 @@ def image_prom():
                       <li class=red>Присоединяйся!</li></br><h3>
                 </html>'''
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+
+        user = User(
+            id=len(session.query(User).all()) + 1,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            addres=form.addres.data,
+            email=form.email.data,
+        )
+        pas = form.password.data
+        user.hassed_password = User.set_password(pas)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+@app.route('/jobs',  methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = JobsForm()
+    if form.validate_on_submit():
+        session = create_session()
+        job = Jobs()
+        job.team_leader = form.team_leader.data
+        job.job = form.job.data
+        job.work_size = form.work_size.data
+        job.collaborators = form.collaborators.data
+        job.is_finished = form.is_finished.data
+        session.add(job)
+        session.commit()
+        return redirect('/')
+    return render_template('jobs.html', title='Adding a job',
+                           form=form)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
--- data/users.py
+++ data/users.py
@@ -1,9 +1,11 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -17,3 +19,9 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
     hassed_password = sqlalchemy.Column(sqlalchemy.String)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def set_password(password):
+        return generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hassed_password, password)
